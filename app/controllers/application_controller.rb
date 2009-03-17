7b27d52785b78d88a36245ad8700b51b7d9a2869
sugi <sugi@cd948371-3e6a-4283-9d1c-98b9d587e4dc>
2009-03-17 17:23:22 +0000

    rails 2.3, jquery-ui 1.7 対応。
    git-svn-id: file:///Users/sugi/Documents/repository/Walkrr@19 cd948371-3e6a-4283-9d1c-98b9d587e4dc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   # session :session_key => '_WalkDBr_session_id'
-  session :disabled => true
+  # session :disabled => true
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,14 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :lib => 'will_paginate'
-  # config.action_controller.session = { :session_key => 'walkrr', :secret => 'sanposanposanposanposanposanposanposanposanpo'}
+  config.action_controller.session = { :session_key => 'walkrr', :secret => 'sanposanposanposanposanposanposanposanposanpo'}
   # Settings in config/environments/* take precedence over those specified here
   
   # Skip frameworks you're not going to use (only works if using vendor/rails)
@@ -31,7 +31,6 @@ Rails::Initializer.run do |config|
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
   # config.action_controller.session_store = :active_record_store
-
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper, 
   # like if you have constraints or database-specific column types
