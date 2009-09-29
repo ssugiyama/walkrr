219d85e15c150636863d8cb624ef7636acb3f038
Shinichi Sugiyama <sugi@chubabook.local>
2009-09-30 03:54:15 +0900

    routing 変更 sqlite DB へのexport rake task public/index.html を削除

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,10 @@ ActionController::Routing::Routes.draw do |map|
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
+#  map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
+  map.connect ':action/:id', :controller => :walks
+#  map.connect ':controller/:action/:id.:format'
+#  map.connect ':controller/:action/:id'
 end
