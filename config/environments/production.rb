4bb223e38a606089c8e61d5e35e1b082aed688d1
Shinichi Sugiyama <sugi@www4261ue.sakura.ne.jp>
2012-09-28 01:27:04 +0900

    some bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Walkrr::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.assets.compress = true
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :scss
 end
